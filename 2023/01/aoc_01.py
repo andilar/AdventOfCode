ef83f22560c8eed2a09e150f0e7e0180a9c54a2b
andilar <andilar@gmail.com>
2023-12-03 11:24:29 +0100

    final solution for day 1

--- 2023/01/aoc_01.py
+++ 2023/01/aoc_01.py
@@ -1,15 +1,29 @@
 import os
 import re
 
-
-file_path = "data.txt"
-coordinates = []
-
+file_path = "/Users/andilar/Documents/GitHub/AdventOfCode/2023/01/data.txt"
+coordinates = [[0,0]]
+final_coord = 0
 
 with open(file_path, "r") as file:
     for line in file:
-        for lin in line:
-            if lin.isdigit():
-                coordinates.append(lin)
-                
+        numbers = re.findall(r'\d+', line)
+        numbers = list(map(int, numbers))
+
+        #hack for getting first and last digit
+        first_num_str = str(numbers[0])
+        first_digit = int(first_num_str[0])
+        last_num_str = str(numbers[-1])
+        last_digit = int(last_num_str[-1])
+
+        row = [first_digit, last_digit]
+        coordinates.append(row)
+
+#build the number    
+concatenated_numbers = [int(''.join(map(str, coord))) for coord in coordinates]
+
+for cn in concatenated_numbers:
+    final_coord += cn
+
+print (final_coord) 
 
