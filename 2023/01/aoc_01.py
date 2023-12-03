05ffa872845d641ffc70896d0d8d6dc8130146bc
andilar <andilar@gmail.com>
2023-12-03 12:16:03 +0100

    Well found a hack online, gold star incoming, shame on me :)

--- 2023/01/aoc_01.py
+++ 2023/01/aoc_01.py
@@ -5,15 +5,15 @@ coordinates = [[0,0]]
 final_coord = 0
 
 replacements = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9"
+    "one": "one1one",
+    "two": "two2two",
+    "three": "three3three",
+    "four": "four4four",
+    "five": "five5five",
+    "six": "six6six",
+    "seven": "seven7seven",
+    "eight": "eight8eight",
+    "nine": "nine9nine"
 }
 
 
