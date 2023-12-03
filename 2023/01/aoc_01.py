e56f96f7b9f5877447deb9b61490a47c37774be1
andilar <andilar@gmail.com>
2023-12-03 12:10:37 +0100

    trying to replace strings with numbers

--- 2023/01/aoc_01.py
+++ 2023/01/aoc_01.py
@@ -4,9 +4,38 @@ file_path = "/Users/andilar/Documents/GitHub/AdventOfCode/2023/01/data.txt"
 coordinates = [[0,0]]
 final_coord = 0
 
+replacements = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+
+def replace_string_with_fitting_number(str,sub,rpl):
+    substring_to_replace = sub
+    replacement_string = rpl
+
+    # Using replace() method with case-insensitive comparison
+    result_string = str.lower().replace(substring_to_replace.lower(), replacement_string)
+
+    print(result_string)
+    return result_string
+
+
 with open(file_path, "r") as file:
     for line in file:
-        # never been good in regex..
+
+        # Iterate through the replacements and apply them
+        for target, replacement in replacements.items():
+            line = replace_string_with_fitting_number(line, target, replacement)
+
+        # never been good in regex though..
         numbers = re.findall(r'\d+', line)
         numbers = list(map(int, numbers))
 
@@ -26,4 +55,4 @@ concatenated_numbers = [int(''.join(map(str, coord))) for coord in coordinates]
 for cn in concatenated_numbers:
     final_coord += cn
 
-print (final_coord) 
+print (final_coord)
