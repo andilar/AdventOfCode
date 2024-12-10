662cae3342848cfecc9b1d9782541bf1f27bd7cd
andilar <andilar@gmail.com>
2024-12-10 16:27:27 +0100

    day08 - still wrong result

---
+++ 2024/day08/src/main.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{ self, BufRead };
+use std::path::Path;
+
+fn parse_input(filename: &str) -> io::Result<Vec<(usize, char)>> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+    let mut antennas = Vec::new();
+
+    for line in io::BufReader::new(file).lines() {
+        let line = line?;
+        for (x, c) in line.chars().enumerate() {
+            if c.is_alphanumeric() {
+                antennas.push((x, c));
+            }
+        }
+    }
+
+    Ok(antennas)
+}
+
+fn find_antinodes(antennas: &[(usize, char)], line_length: usize) -> HashSet<usize> {
+    let mut antinodes = HashSet::new();
+
+    for i in 0..antennas.len() {
+        for j in i + 1..antennas.len() {
+            let (pos1, freq1) = antennas[i];
+            let (pos2, freq2) = antennas[j];
+
+            if freq1 == freq2 {
+                let distance = ((pos2 as isize) - (pos1 as isize)).abs() as usize;
+                if distance % 2 == 0 {
+                    let mid = (pos1 + pos2) / 2;
+                    antinodes.insert(mid);
+                    let antinode1 = (pos1 as isize) - (distance as isize) / 2;
+                    let antinode2 = (pos2 as isize) + (distance as isize) / 2;
+                    if antinode1 >= 0 {
+                        antinodes.insert(antinode1 as usize);
+                    }
+                    if antinode2 < (line_length as isize) {
+                        antinodes.insert(antinode2 as usize);
+                    }
+                }
+            }
+        }
+    }
+
+    antinodes
+}
+
+fn main() -> io::Result<()> {
+    let antennas = parse_input("input.txt")?;
+    let line_length =
+        antennas
+            .iter()
+            .map(|(pos, _)| *pos)
+            .max()
+            .unwrap_or(0) + 1;
+    let antinodes = find_antinodes(&antennas, line_length);
+    println!("Number of unique antinode locations: {}", antinodes.len());
+    Ok(())
+}
